17b31dca647261024cd7e7ef2a13753abff22920
YoungJung Kim <jigm1539@oppabox.com>
2014-11-14 17:53:17 +0900

    배송안내, 환불안내 수정

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,12 +17,62 @@ b = Box.where(:path => "star_box").first
 #이민호박스
 
 #JEWELRY BOX
-b = Box.where(:path => "jewelry_box").first
 #SET 1
+b = Box.where(:path => "jewelry_box").first
+i = Item.new
+	i.box = b
+	i.path = "cross"
+	i.logo_path = "/items/jewelry_box/cross/cross.jpg"
+	i.original_price = ""
+	i.sale_price = ""
+	i.show_original_price = true
+	i.quantity = -1
+	i.limited = false
+i.save
+
+#SET 1 : LOCALE NAME
+n = ItemName.new
+n.item = i
+n.locale = "ko"
+n.name = "925실버 크로스 이니셜팔찌"
+n.save
+#SET 1 : IMAGES
+m = ItemImage.new
+m.item = i
+m.path = "/items/jewelry_box/cross/main.jpg"
+m.save
+
+#SET 1 : OPTIONS
+#NONE
+
 #SET 2
-#SET 3
-#SET 4
-#SET 5
+b = Box.where(:path => "jewelry_box").first
+i = Item.new
+  i.box = b
+  i.path = "knot"
+  i.logo_path = "/items/jewelry_box/knot/knot.jpg"
+  i.original_price = ""
+  i.sale_price = ""
+  i.show_original_price = true
+  i.quantity = -1
+  i.limited = false
+i.save
+
+#SET 2 : LOCALE NAME
+n = ItemName.new
+n.item = i
+n.locale = "ko"
+n.name = "925실버 매듭 셋트"
+n.save
+
+#SET 2 : IMAGES
+m = ItemImage.new
+m.item = i
+m.path = "/items/jewelry_box/knot/main.jpg"
+m.save
+
+#SET 2 : OPTIONS
+#NONE
 
 #BEAUTY BOX
 #SET 1 : BASIC INFO
