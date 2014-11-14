802092d4e74179a8174ec60aaf8bc05eca200e7a
cycorld <choi@likelion.net>
2014-11-14 17:07:12 +0900

    purchase 모델 생성 및 order 내용 정리

--- db/migrate/20141107034217_create_orders.rb
+++ db/migrate/20141107034217_create_orders.rb
@@ -4,17 +4,6 @@ class CreateOrders < ActiveRecord::Migration
       t.integer     :purchase_id
       t.integer     :item_id
       t.integer     :quantity, :null => false, :default => 1
-      t.integer     :user_id
-
-      t.string      :recipient
-      t.string      :country
-      t.string      :address_1
-      t.string      :address_2
-      t.string      :address_3
-      t.string      :postcode
-      t.string      :phonenumber
-
-      t.string      :comment
 
       t.timestamps
     end
