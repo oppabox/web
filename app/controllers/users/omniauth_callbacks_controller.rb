3535844ceb8feeae2ddaa44217d0af786a841ec0
parksusang <sang2087@gmail.com>
2014-11-29 14:27:44 +0900

    웨이보 로그인 기능 추가

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,14 +1,41 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  def facebook
-    # You need to implement the method below in your model (e.g. app/models/user.rb)
-    @user = User.from_omniauth(request.env["omniauth.auth"])
 
-    if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
-    else
-      session["devise.facebook_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
+    def all
+      user = User.from_omniauth(request.env["omniauth.auth"])
+      if user.persisted?
+        sign_in_and_redirect user, notice: 'Success'
+      else
+        session["devise.user_attributes"] = user.attributes
+        redirect_to new_user_registration_url
+      end
     end
-  end
+  
+    alias_method :facebook, :all
+    alias_method :weibo, :all
+  # def facebook
+  #   # You need to implement the method below in your model (e.g. app/models/user.rb)
+  #   @user = User.from_omniauth(request.env["omniauth.auth"])
+
+  #   if @user.persisted?
+  #     sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+  #     set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+  #   else
+  #     session["devise.facebook_data"] = request.env["omniauth.auth"]
+  #     redirect_to new_user_registration_url
+  #   end
+  # end
+
+  # def weibo
+  #   # You need to implement the method below in your model (e.g. app/models/user.rb)
+  #   @user = User.from_omniauth(request.env["omniauth.auth"])
+
+  #   if @user.persisted?
+  #     sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+  #     set_flash_message(:notice, :success, :kind => "Weibo") if is_navigational_format?
+  #   else
+  #     session["devise.weibo_data"] = request.env["omniauth.auth"]
+  #     redirect_to new_user_registration_url
+  #   end
+  # end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,20 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, :omniauth_providers => [:facebook]
+         :omniauthable, :omniauth_providers => [:facebook, :weibo]
 
   has_many :purchases
   has_many :baskets
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
+      binding.pry
+      user.email = 
+        if auth.info.email.nil?
+          "#{auth.uid}@weibo.com"
+        else
+          auth.info.email
+        end
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name   # assuming the user model has a name
       #user.image = auth.info.image # assuming the user model has an image
@@ -18,9 +24,19 @@ class User < ActiveRecord::Base
   end
 
   def self.new_with_session(params, session)
-    super.tap do |user|
-      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-        user.email = data["email"] if user.email.blank?
+    # super.tap do |user|
+    #   if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+    #     user.email = data["email"] if user.email.blank?
+    #   end
+    # end
+    def self.new_with_session(params, session)
+      if session["devise.user_attributes"]
+        new(session["devise.user_attributes"], without_protection: true) do |user|
+          user.attributes = params
+          user.valid?
+        end
+      else
+        super
       end
     end
   end
