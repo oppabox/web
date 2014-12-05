ee3d095016a5708c199dd3d4f54ad0f83cce93b3
iamdoo2 <iamdoo2@gmail.com>
2014-12-05 20:08:52 +0900

    푸터정리

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  constraints subdomain: false do
-    get ':any', to: redirect(subdomain: 'www', path: '/%{any}'), any: /.*/
-  end
 
   devise_for :admin_users, ActiveAdmin::Devise.config
 
