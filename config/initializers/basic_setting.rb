14b10c389ec756f5d915ccb33825ee5e5d9650a3
Kim Young Jung <jigm1539@oppabox.com>
2014-12-12 11:30:59 +0900

    배너이미지 바꾸기

--- config/initializers/basic_setting.rb
+++ config/initializers/basic_setting.rb
@@ -1,2 +1,9 @@
-HOME_IMAGE = "/images/top/1.jpg"
+MAIN_BANR = "/images/top/1.jpg"
+STAR_BANR = "/images/top/2.jpg"
+DESIGN_BANR = "/images/top/3.jpg"
+BEAUTY_BANR = "/images/top/4.jpg"
+KITCHEN_BANR = "/images/top/5.jpg"
+GRIENICH_BANR = "/images/top/6.jpg"
+JEWELRY_BANR = "/images/top/7.jpg"
+
 BASE_ITEM_IMAGE_LOCATION = "/images/items"
