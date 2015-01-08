0258b28b62c61beadcfd68b103624a5db948ce18
Seok jun Seo <seokjunseo@Seokui-MacBook-Pro.local>
2015-01-08 12:21:16 +0900

    csv exporter 추가

--- app/admin/order.rb
+++ app/admin/order.rb
@@ -21,6 +21,142 @@ ActiveAdmin.register Order do
 
     redirect_to :action => :index
   end
+  ########### download bl #############
+  collection_action :download_bl do
+    csv_builder = ActiveAdmin::CSVBuilder.new
+
+    # set columns
+    csv_builder.column("ReceiverCompanyName") { |o| o.purchase.recipient }
+    csv_builder.column("ReceiverContactName") { |o| o.purchase.recipient }
+    csv_builder.column("ReceiverAddress1") do |o|
+      address_array = o.purchase.address.split(" ")
+      address_split = Array.new(3){""}
+
+      address_array.each do |x|
+        0.upto(address_split.size - 1) do |y|
+          next if (address_split[y] + x).size > 35 #UPS can have address with legnth 35.
+          address_split[y] += " " if address_split[y].size > 1
+          address_split[y] += x
+          break
+        end
+      end
+
+      address_split[0]
+    end
+    csv_builder.column("ReceiverAddress2") do |o|
+      address_array = o.purchase.address.split(" ")
+      address_split = Array.new(3){""}
+
+      address_array.each do |x|
+        0.upto(address_split.size - 1) do |y|
+          next if (address_split[y] + x).size > 35 #UPS can have address with legnth 35.
+          address_split[y] += " " if address_split[y].size > 1
+          address_split[y] += x
+          break
+        end
+      end
+
+      address_split[1]
+    end
+    csv_builder.column("ReceiverAddress3") do |o|
+      address_array = o.purchase.address.split(" ")
+      address_split = Array.new(3){""}
+
+      address_array.each do |x|
+        0.upto(address_split.size - 1) do |y|
+          next if (address_split[y] + x).size > 35 #UPS can have address with legnth 35.
+          address_split[y] += " " if address_split[y].size > 1
+          address_split[y] += x
+          break
+        end
+      end
+
+      address_split[2]
+    end
+    csv_builder.column("ReceiverCity") { |o| o.purchase.city }
+    csv_builder.column("ReceiverState") { |o| o.purchase.state }
+    csv_builder.column("ReceiverPostal") { |o| o.purchase.postcode }
+    csv_builder.column("ReceiverCountry") { |o| o.purchase.user.country }
+    csv_builder.column("ReceiverPhoneNumber") { |o| o.purchase.phonenumber }
+    csv_builder.column("ReceiverEmail") { |o| o.purchase.user.email }
+    csv_builder.column("DescriptionofShipment") { |o| o.item.display_name }
+    csv_builder.column("YOption") { |o| "Y" }
+    csv_builder.column("NOption") { |o| "N" }
+    csv_builder.column("ServiceType") { |o| "Express Saver" }
+    csv_builder.column("ActWeight") { |o| o.item.weight * o.quantity }
+    csv_builder.column("NumofPackage") { |o| "1" }
+    csv_builder.column("PackageType") { |o| "Package" }
+    csv_builder.column("ShippingChargeto") { |o| "Shipper" }
+    csv_builder.column("ShippingTaxto") { |o| "Receiver" }
+    csv_builder.column("ReferenceNumber1") { |o| "refnum" } 
+    csv_builder.column("ReferenceNumber2") { |o| "" }
+    csv_builder.column("ReferenceNumber3") { |o| "" }
+    csv_builder.column("ReferenceNumber4") { |o| "" }
+    csv_builder.column("ReferenceNumber5") { |o| "Saver" }
+
+
+    columns = csv_builder.columns
+
+    data = []
+    data << columns.map(&:name)
+    collection.each do |resource|
+      data << columns.map do |column|
+        call_method_or_proc_on resource, column.data
+      end
+    end
+
+    csv_output = CSV.generate() do |csv|
+      data.each do |row|
+        csv << row
+      end
+    end
+
+    # render plain: csv_output.inspect
+
+    send_data csv_output, :filename => "Shipinfo_BL.csv"
+  end
+  ########### download inv #############
+  collection_action :download_inv do
+    csv_builder = ActiveAdmin::CSVBuilder.new
+
+    # set columns
+    csv_builder.column("ReferenceNumber1") { |o| "refnum" } 
+    csv_builder.column("INVCurrency") { |o| "USD" }
+    csv_builder.column("INVDeclaration") { |o| "invoice" }
+    csv_builder.column("INVReasonforExport") { |o| "Sale" }
+    csv_builder.column("INVDescriptionofGoods") { |o| o.item.display_name }
+    csv_builder.column("INVHsCode") { |o| "" }
+    csv_builder.column("INVOriginCountry") { |o| "KR" }
+    csv_builder.column("INVQuantity") { |o| "" }
+    csv_builder.column("INVUnitofMeasure") { |o| "EA" }
+    csv_builder.column("INVUnitPrice") { |o| "" }
+    csv_builder.column("INVAddComment") { |o| "" }
+    csv_builder.column("INVFleightCost") { |o| "" }
+    csv_builder.column("INVDiscountCost") { |o| "" }
+    
+
+    columns = csv_builder.columns
+
+    # Collect the data in an Array to be transposed.
+    data = []
+    data << columns.map(&:name)
+    collection.each do |resource|
+      data << columns.map do |column|
+        call_method_or_proc_on resource, column.data
+      end
+    end
+
+    csv_output = CSV.generate() do |csv|
+      data.each do |row|
+        csv << row
+      end
+    end
+
+    # render plain: csv_output.inspect
+
+    send_data csv_output, :filename => "Shipinfo_INV.csv"
+  end
+
 
 
 
@@ -88,10 +224,19 @@ ActiveAdmin.register Order do
     column :updated_at
     column :created_at
     column "Done" do |o|
-      link_to "done", { :action => :change_status_to_done, :id => o.id }, {:class => "done_btn", :style => "border-radius: 4px;font-size: 14px;font-weight: bold;line-height: 200%;text-decoration: none !important;background: white;background: -webkit-linear-gradient(-90deg, white, #e7e7e7);background: -moz-linear-gradient(-90deg, white, #e7e7e7);background: linear, 180deg, white, #e7e7e7;box-shadow: 0 1px 1px rgba(0, 0, 0, 0.1), 0 1px 0 0 rgba(255, 255, 255, 0.8) inset;border: solid 1px #c7c7c7;border-color: #c7c7c7;padding: 3px 5px;color: #5e6469 !important;"}
+      link_to "done", { :action => :change_status_to_done, :id => o.id }, {:style => "border-radius: 4px;font-size: 14px;font-weight: bold;line-height: 200%;text-decoration: none !important;background: white;background: -webkit-linear-gradient(-90deg, white, #e7e7e7);background: -moz-linear-gradient(-90deg, white, #e7e7e7);background: linear, 180deg, white, #e7e7e7;box-shadow: 0 1px 1px rgba(0, 0, 0, 0.1), 0 1px 0 0 rgba(255, 255, 255, 0.8) inset;border: solid 1px #c7c7c7;border-color: #c7c7c7;padding: 3px 5px;color: #5e6469 !important;"}
     end
     column "Cancel" do |o|
-      link_to "return", { :action => :cancel, :id => o.id }, {:class => "done_btn", :style => "border-radius: 4px;font-size: 14px;letter-spacing: 0.5px;line-height: 200%;text-decoration: none !important;background: #d45f53;background: -webkit-linear-gradient(-90deg, #d45f53, #d05a49);background: -moz-linear-gradient(-90deg, #d45f53, #d05a49);background: linear, 180deg, #d45f53, #d05a49;border: solid 1px #b43f33;border-color: #b43f33;padding: 3px 5px;color: #ffffff !important;"}
+      link_to "return", { :action => :cancel, :id => o.id }, {:style => "border-radius: 4px;font-size: 14px;letter-spacing: 0.5px;line-height: 200%;text-decoration: none !important;background: #d45f53;background: -webkit-linear-gradient(-90deg, #d45f53, #d05a49);background: -moz-linear-gradient(-90deg, #d45f53, #d05a49);background: linear, 180deg, #d45f53, #d05a49;border: solid 1px #b43f33;border-color: #b43f33;padding: 3px 5px;color: #ffffff !important;"}
+    end
+  end
+
+  sidebar :help, :only => :index do
+    button do
+      link_to "Download BL", { :action => :download_bl }, {:style => "color:white;text-decoration:none"}
+    end
+    button do
+      link_to "Download INV", { :action => :download_inv }, {:style => "color:white;text-decoration:none"}
     end
   end
 
@@ -145,10 +290,10 @@ ActiveAdmin.register Order do
           row :updated_at
           row :created_at
           row "Done" do |o|
-            link_to "done", { :action => :change_status_to_done, :id => o.id }, {:class => "done_btn", :style => "border-radius: 4px;font-size: 14px;font-weight: bold;line-height: 200%;text-decoration: none !important;background: white;background: -webkit-linear-gradient(-90deg, white, #e7e7e7);background: -moz-linear-gradient(-90deg, white, #e7e7e7);background: linear, 180deg, white, #e7e7e7;box-shadow: 0 1px 1px rgba(0, 0, 0, 0.1), 0 1px 0 0 rgba(255, 255, 255, 0.8) inset;border: solid 1px #c7c7c7;border-color: #c7c7c7;padding: 3px 5px;color: #5e6469 !important;"}
+            link_to "done", { :action => :change_status_to_done, :id => o.id }, {:style => "border-radius: 4px;font-size: 14px;font-weight: bold;line-height: 200%;text-decoration: none !important;background: white;background: -webkit-linear-gradient(-90deg, white, #e7e7e7);background: -moz-linear-gradient(-90deg, white, #e7e7e7);background: linear, 180deg, white, #e7e7e7;box-shadow: 0 1px 1px rgba(0, 0, 0, 0.1), 0 1px 0 0 rgba(255, 255, 255, 0.8) inset;border: solid 1px #c7c7c7;border-color: #c7c7c7;padding: 3px 5px;color: #5e6469 !important;"}
           end
           row "Cancel" do |o|
-            link_to "return", { :action => :cancel, :id => o.id }, {:class => "done_btn", :style => "border-radius: 4px;font-size: 14px;letter-spacing: 0.5px;line-height: 200%;text-decoration: none !important;background: #d45f53;background: -webkit-linear-gradient(-90deg, #d45f53, #d05a49);background: -moz-linear-gradient(-90deg, #d45f53, #d05a49);background: linear, 180deg, #d45f53, #d05a49;border: solid 1px #b43f33;border-color: #b43f33;padding: 3px 5px;color: #ffffff !important;"}
+            link_to "return", { :action => :cancel, :id => o.id }, {:style => "border-radius: 4px;font-size: 14px;letter-spacing: 0.5px;line-height: 200%;text-decoration: none !important;background: #d45f53;background: -webkit-linear-gradient(-90deg, #d45f53, #d05a49);background: -moz-linear-gradient(-90deg, #d45f53, #d05a49);background: linear, 180deg, #d45f53, #d05a49;border: solid 1px #b43f33;border-color: #b43f33;padding: 3px 5px;color: #ffffff !important;"}
           end
         end
       end
@@ -208,6 +353,7 @@ ActiveAdmin.register Order do
     column :updated_at
     column :created_at
   end
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
