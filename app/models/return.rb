86f289a235c0eeb35bae02117025a13dc7ef8680
Kim Young Jung <jigm1539@oppabox.com>
2015-01-13 18:12:55 +0900

    return locale apply

--- app/models/return.rb
+++ app/models/return.rb
@@ -14,15 +14,15 @@ class Return < ActiveRecord::Base
 	}
 
 	REASONS = {
-		0 => "Please select the reason",
-		1 => "No longer needed/wanted",
-		2 => "Different from website description",
-		3 => "Select options incorrectly",
-		4 => "Damaged due to inappropriate packaging",
-		5 => "Missed estimated delivery date",
-		6 => "Different from what was ordered",
-		7 => "Defective/Does not work properly",
-		8 => "Etc."
+		0 => 'return_model_reasons0',
+		1 => 'return_model_reasons1',
+		2 => 'return_model_reasons2',
+		3 => 'return_model_reasons3',
+		4 => 'return_model_reasons4',
+		5 => 'return_model_reasons5',
+		6 => 'return_model_reasons6',
+		7 => 'return_model_reasons7',
+		8 => 'return_model_reasons8'
 	}
 
 	def quantity_check
