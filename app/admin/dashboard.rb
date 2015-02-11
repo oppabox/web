09131867ee9b4104d2d7a5cc174bc322f078c6f1
Seok jun Seo <seokjunseo@Seokui-MacBook-Pro.local>
2015-02-11 15:21:52 +0900

    temp

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -31,8 +31,6 @@ ActiveAdmin.register_page "Dashboard" do
 		res = []
 		query.keys.each do |k|
 			res << [DateTime.strptime(k, "%Y%m%d").to_i * 1000, query[k]]
-			res << [(DateTime.strptime(k, "%Y%m%d")-1).to_i * 1000, query[k]+1]
-			res << [(DateTime.strptime(k, "%Y%m%d")-5).to_i * 1000, query[k]+3]
 		end
 		
 		render json: { range: diff, data: res }
